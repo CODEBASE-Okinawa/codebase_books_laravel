028ec9d312c0d6c9b67ce6f7c551be8997b4045a
onaga <yu.xxa.on@gmail.com>
2023-02-13 21:26:42 +0900

    不要コード削除

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    <img src="{{ asset('storage/r1166.png') }}" alt="">
                 </div>
             </div>
         </div>

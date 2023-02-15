e45307021219aa74ce6d118a85b578080f834bd8
onaga <yu.xxa.on@gmail.com>
2023-02-15 18:38:55 +0900

    reservation-routing追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Reservation;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    //reservationルーティング
+    Route::post('/reservations',[ReservationController::class,'store'])->name('reservation.store');
+    Route::get('/reservations/{reservationId}',[ReservationController::class,'show'])->name('reservation.show');
 });
 
 require __DIR__.'/auth.php';

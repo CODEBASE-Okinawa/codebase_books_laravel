d0ca589909d38ead80328372fbd6efc725781d1a
Juri Matsuda <matsuda.juri@gmail.com>
2023-02-23 15:34:12 +0900

    貸出一覧、詳細のレイアウト調整

--- resources/views/lending/index.blade.php
+++ resources/views/lending/index.blade.php
@@ -1,16 +1,36 @@
-<h1>借りている本一覧</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('貸出一覧') }}
+            </h2>
 
-@foreach ( $lendings as $lending )
+            <div>
+                <a href="{{ url()->previous() }}">戻る</a>
+            </div>
+        </div>
+    </x-slot>
 
-    <a href="{{ route('lending.show', ['lendingId' => $lending->id]) }}">
-        <img src="{{ asset('storage/'.$lending->book->image_path) }}" alt="">
+    <div class="py-12">
+        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex justify-between">
+                    @foreach ( $lendings as $lending )
+                        <div class="w-1/5">
+                            <a href="{{ route('lending.show', ['lendingId' => $lending->id]) }}">
+                                <img class="object-cover w-full" src="{{ asset('storage/'.$lending->book->image_path) }}"
+                                     alt="book">
+                                <ul class="bg-zinc-400 p-3">
+                                    <li class="text-2xl font-bold">{{ $lending->book->title }}</li>
+                                    <li>{{ \Carbon\Carbon::parse($lending->end_at)->format('Y年m月d日')}}まで</li>
+                                </ul>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <h3>{{ $lending->book->title }}</h3>
-        <h3>{{ \Carbon\Carbon::parse($lending->end_at)->format('Y年m月d日')}}まで</h3>
-    </a>
+</x-app-layout>
 
-@endforeach
-
-<div>
-    <a href="{{ url()->previous() }}">戻る</a>
-</div>
--- resources/views/lending/show.blade.php
+++ resources/views/lending/show.blade.php
@@ -1,28 +1,42 @@
-<h1>借りている本詳細</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('貸出一覧') }} > {{ $lending->book->title }}
+            </h2>
 
-<a href="{{ route('lending.show', ['lendingId' => $lending->id]) }}">
-  <img src="{{ asset('storage/'.$lending->book->image_path) }}" alt="">
+            <div>
+                <a href="{{ url()->previous() }}">戻る</a>
+            </div>
+        </div>
+    </x-slot>
 
-  <h3>{{ $lending->book->title }}</h3>
+    <div class="py-12">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex justify-between">
+                    <img src="{{ asset('storage/'.$lending->book->image_path) }}" alt="">
+                    <div class="w-1/2">
+                        <p class="text-4xl font-bold mb-4">{{ $lending->book->title }}</p>
+                        <p class="inline-block font-bold bg-sky-500 px-8">現在借りています</p>
 
-</a>
+                        <form class="mt-10" action="{{ route('lending.updateIsReturned', ['lendingId' => $lending->id]) }}" method="post">
+                            @csrf
+                            @method('PUT')
+                            <input class="w-full bg-red-600 text-white font-bold py-3" type="submit" value="返却する">
+                        </form>
 
-<form action="{{ route('lending.updateIsReturned', ['lendingId' => $lending->id]) }}" method="post">
-  @csrf
-  @method('PUT')
-  <input type="submit" value="返却する">
-</form>
-
-@if ($lending->end_at < $now)
-
-  <p>	返却期間過ぎています！</p>
-
-@else()
-
-  <p>{{ \Carbon\Carbon::parse($lending->end_at)->format('Y年m月d日 H:i')}}まで借りています</p>
-
-@endif
-
-<div>
-    <a href="{{ url()->previous() }}">戻る</a>
-</div>
+                        <div class="mt-10">
+                            @if ($lending->end_at < $now)
+                                <p class="font-bold text-4xl">返却日が過ぎています！</p>
+                                <p class="font-bold text-3xl mt-8">早く返却しないとジュリさんが追いかけてくるので注意してください！！</p>
+                            @else
+                                <p class="font-bold">{{ \Carbon\Carbon::parse($lending->end_at)->format('Y年m月d日 H:i')}}まで借りています</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

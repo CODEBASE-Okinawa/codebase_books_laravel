6467d5f54e1a06d895adc3595975d06f12bfef81
Juri Matsuda <matsuda.juri@gmail.com>
2023-02-23 15:11:33 +0900

    本詳細画面のレイアウト調整

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -1,16 +1,44 @@
-<h1>{{ $book->title }}</h1>
-<img src="{{ asset('storage/'.$book->image_path) }}" alt="">
-<div>
-    <form id="two-destinations-form">
-        @csrf
-        <input type="date" name="start_at">から
-        <input type="date" name="end_at">まで
-        <input type="hidden" name="book_id" value="{{ $book->id }}">
-        <input type="hidden" name="is_returned" value="0">
-        <button type="submit" name="lending">借りる</button>
-        <button type="submit" name="reservation">予約する</button>
-    </form>
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('本一覧') }} > {{ $book->title }}
+            </h2>
+
+            <div>
+                <a href="{{ url()->previous() }}">戻る</a>
+            </div>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex justify-between">
+                    <img src="{{ asset('storage/'.$book->image_path) }}" alt="">
+                    <div>
+                        <p class="text-4xl font-bold mb-4">{{ $book->title }}</p>
+                        <p class="inline-block font-bold bg-lime-500 px-8">貸出可能</p>
+
+                        <form class="mt-10" id="two-destinations-form">
+                            @csrf
+                            <input type="date" name="start_at" class="w-4/5 mr-2 mb-3"><span class="text-2xl">から</span>
+                            <input type="date" name="end_at" class="w-4/5 mr-2"><span class="text-2xl">まで</span>
+
+                            <input type="hidden" name="book_id" value="{{ $book->id }}">
+                            <input type="hidden" name="is_returned" value="0">
+
+                            <div class="w-4/5 flex justify-between mt-10">
+                                <button type="submit" name="lending" class="w-1/3 bg-sky-500 font-bold py-2">借りる</button>
+                                <button type="submit" name="reservation" class="w-1/3 bg-yellow-300 font-bold py-2">予約する</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
 <div>
     <a href="{{ url()->previous() }}">戻る</a>

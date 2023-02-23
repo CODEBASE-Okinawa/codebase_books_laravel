b7ac0536acd4841e7971f3e46cd5fbcb1b284d94
Juri Matsuda <matsuda.juri@gmail.com>
2023-02-23 17:18:07 +0900

    予約一覧、詳細のレイアウト調整

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -1,24 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('予約している本一覧') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('予約一覧') }}
+            </h2>
+
+            <div>
+                <a href="{{ url()->previous() }}">戻る</a>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-sm sm:rounded-lg flex justify-around">
-                @foreach ( $reservations as $reservation)
-                    <div class="text-gray-900 w-1/5 bg-zinc-400">
-                        <a href="{{ route('reservation.show', ['reservationId' => $reservation->id]) }}">
-                            <img class="object-cover w-full" src="{{ asset('storage/' . $reservation->book->image_path) }}" alt="">
-                            <div class="p-3">
-                                <p class="text-2xl font-bold">{{ $reservation->book->title }}</p>
-                                <p>{{ \Carbon\Carbon::parse($reservation->start_at)->format('Y年m月d日')}}から</p>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
+        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex justify-between">
+                    @foreach ( $reservations as $reservation)
+                        <div class="w-1/5">
+                            <a href="{{ route('reservation.show', ['reservationId' => $reservation->id]) }}">
+                                <img class="object-cover w-full" src="{{ asset('storage/' . $reservation->book->image_path) }}"
+                                     alt="book">
+                                <ul class="bg-zinc-400 p-3">
+                                    <li class="text-2xl font-bold">{{ $reservation->book->title }}</li>
+                                    <li>{{ \Carbon\Carbon::parse($reservation->start_at)->format('Y年m月d日')}}まで</li>
+                                </ul>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
--- resources/views/reservation/show.blade.php
+++ resources/views/reservation/show.blade.php
@@ -1,30 +1,38 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('予約している本一覧') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('予約一覧') }} > {{ $reservation->book->title }}
+            </h2>
+
+            <div>
+                <a href="{{ url()->previous() }}">戻る</a>
+            </div>
+        </div>
     </x-slot>
 
-    <a href="{{ url()->previous() }}">戻る</a>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden sm:rounded-lg flex justify-around">
-                <div class="text-gray-900 w-1/5 bg-zinc-400">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex justify-between">
+                    <img class="object-cover w-1/3" src="{{ asset('storage/' . $reservation->book->image_path) }}" alt="">
+                    <div class="w-1/2">
+                        <p class="text-4xl font-bold mb-4">{{ $reservation->book->title }}</p>
+                        <p class="inline-block font-bold bg-yellow-300 px-8">予約しています</p>
 
-                    <img src="{{ asset('storage/' . $reservation->book->image_path) }}" alt="">
+                        <form class="mt-10" action="{{ route('reservation.destroy', ['reservationId' => $reservation->id]) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <input class="w-full bg-red-600 text-white font-bold py-3" type="submit" value="予約を取り消す">
+                        </form>
 
-                    <h3>{{ $reservation->book->title }}</h3>
-                    <h3>{{ \Carbon\Carbon::parse($reservation->start_at)->toDateString()}}
-                        ~ {{ \Carbon\Carbon::parse($reservation->end_at)->toDateString()}}</h3>
-
-                    <form action="{{ route('reservation.destroy', ['reservationId' => $reservation->id]) }}"
-                          method="post">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="予約を取り消す">
-                    </form>
-
+                        <div class="mt-10">
+                            <p class="text-xl">以下の日程で予約しています</p>
+                            <p class="font-bold text-2xl">{{ \Carbon\Carbon::parse($reservation->start_at)->toDateString()}}
+                                ~ {{ \Carbon\Carbon::parse($reservation->end_at)->toDateString()}}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

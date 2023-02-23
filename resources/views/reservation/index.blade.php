45ad5da5e05f437ca07ee91738a5e2a9f70fac97
Juri Matsuda <matsuda.juri@gmail.com>
2023-02-23 12:52:26 +0900

    [WIP]

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -1,12 +1,26 @@
-<h1>予約している本一覧</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('予約している本一覧') }}
+        </h2>
+    </x-slot>
 
-@foreach ( $reservations as $reservation)
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden shadow-sm sm:rounded-lg flex justify-around">
+                @foreach ( $reservations as $reservation)
+                    <div class="text-gray-900 w-1/5 bg-zinc-400">
+                        <a href="{{ route('reservation.show', ['reservationId' => $reservation->id]) }}">
+                            <img class="object-cover w-full" src="{{ asset('storage/' . $reservation->book->image_path) }}" alt="">
+                            <div class="p-3">
+                                <p class="text-2xl font-bold">{{ $reservation->book->title }}</p>
+                                <p>{{ \Carbon\Carbon::parse($reservation->start_at)->format('Y年m月d日')}}から</p>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 
-  <a href="{{ route('reservation.show', ['reservationId' => $reservation->id]) }}">
-    <img src="{{ asset($reservation->book->image_path) }}" alt="">
-
-    <h3>{{ $reservation->book->title }}</h3>
-    <h3>{{ \Carbon\Carbon::parse($reservation->start_at)->format('Y年m月d日')}}から</h3>
-  </a>
-  
-@endforeach
+</x-app-layout>
--- resources/views/reservation/show.blade.php
+++ resources/views/reservation/show.blade.php
@@ -1,12 +1,32 @@
-<a href="">
-  <img src="{{ asset($reservation->book->image_path) }}" alt="">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('予約している本一覧') }}
+        </h2>
+    </x-slot>
 
-  <h3>{{ $reservation->book->title }}</h3>
-  <h3>{{ \Carbon\Carbon::parse($reservation->start_at)->toDateString()}} ~ {{ \Carbon\Carbon::parse($reservation->end_at)->toDateString()}}</h3>
-</a>
+    <a href="{{ url()->previous() }}">戻る</a>
 
-<form action="{{ route('reservation.destroy', ['reservationId' => $reservation->id]) }}" method="post">
-  @csrf
-  @method('DELETE')
-  <input type="submit" value="予約を取り消す">
-</form>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden sm:rounded-lg flex justify-around">
+                <div class="text-gray-900 w-1/5 bg-zinc-400">
+
+                    <img src="{{ asset('storage/' . $reservation->book->image_path) }}" alt="">
+
+                    <h3>{{ $reservation->book->title }}</h3>
+                    <h3>{{ \Carbon\Carbon::parse($reservation->start_at)->toDateString()}}
+                        ~ {{ \Carbon\Carbon::parse($reservation->end_at)->toDateString()}}</h3>
+
+                    <form action="{{ route('reservation.destroy', ['reservationId' => $reservation->id]) }}"
+                          method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="予約を取り消す">
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

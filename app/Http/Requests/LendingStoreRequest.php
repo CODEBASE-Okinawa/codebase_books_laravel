851943eb0a4b5764d1aa9effbedcb0967d5f4baf
Juri Matsuda <matsuda.juri@gmail.com>
2023-02-25 15:53:48 +0900

    バリデーション完成！

--- app/Http/Requests/LendingStoreRequest.php
+++ app/Http/Requests/LendingStoreRequest.php
@@ -25,20 +25,20 @@ class LendingStoreRequest extends FormRequest
     public function rules(Request $request)
     {
         return [
-            
-            'start_at' => 'required|date|before:now',
-            'end_at' => 'required|date|after:start_at'
+
+            'start_at' => 'required|date|after:now',
+            'end_at'   => 'required|date|after:start_at'
 
         ];
     }
 
     public function messages()
     {
-       return [
-        'start_at.required' => '貸出開始日は必須です',
-        'start_at.before' => '貸出開始日は本日以降で入力してください',
-        'end_at.required' => '貸出終了日は必須です',
-        'end_at.after' => '貸出終了日は予約開始日以降で入力してください'
-       ];
+        return [
+            'start_at.required' => '貸出開始日は必須です',
+            'start_at.after'    => '貸出開始日は本日以降で入力してください',
+            'end_at.required'   => '貸出終了日は必須です',
+            'end_at.after'      => '貸出終了日は予約開始日以降で入力してください'
+        ];
     }
 }
--- app/Http/Requests/ReservationStoreRequest.php
+++ app/Http/Requests/ReservationStoreRequest.php
@@ -26,19 +26,19 @@ class ReservationStoreRequest extends FormRequest
     public function rules(Request $request)
     {
         return [
-            'start_at' => 'required|date|before:now',
-            'end_at' => 'required|date|after:start_at'
+            'start_at' => 'required|date|after:now',
+            'end_at'   => 'required|date|after:start_at'
 
         ];
     }
 
     public function messages()
     {
-       return [
-        'start_at.required' => '予約開始日は必須です',
-        'start_at.before' => '予約開始日は本日以降で入力してください',
-        'end_at.required' => '予約終了日は必須です',
-        'end_at.after' => '予約終了日は予約開始日以降で入力してください'
-       ];
+        return [
+            'start_at.required' => '予約開始日は必須です',
+            'start_at.after'    => '予約開始日は本日以降で入力してください',
+            'end_at.required'   => '予約終了日は必須です',
+            'end_at.after'      => '予約終了日は予約開始日以降で入力してください'
+        ];
     }
 }
